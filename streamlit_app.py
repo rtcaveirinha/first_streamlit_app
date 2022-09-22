75a4bb83f940db15d8f95b7a573d069d153f9483
rtcaveirinha <113922856+rtcaveirinha@users.noreply.github.com>
2022-09-22 11:20:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,10 @@ my_cur = my_cnx.cursor()
 my_cur.execute("use warehouse pc_rivery_wh")
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
+
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 streamlit.text("The fruit load list contains")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 
