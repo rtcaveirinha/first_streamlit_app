e1ea84a9474d2a93b879abc56554d3f4c239ec93
rtcaveirinha <113922856+rtcaveirinha@users.noreply.github.com>
2022-09-22 11:49:02 +0100

    Update streamlit_app.py
    fiz typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def get_fruit_load_list():
       my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
       return my_cur.fetchall()
  
-if streamlit.buttom('Get Fruit Load Lits'):
+if streamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
