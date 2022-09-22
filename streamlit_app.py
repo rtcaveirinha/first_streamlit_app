bc0b10cee886d4d13edb6a30ad2f429c3195add9
rtcaveirinha <113922856+rtcaveirinha@users.noreply.github.com>
2022-09-22 11:57:34 +0100

    Update streamlit_app.py
    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,12 +59,21 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 streamlit.stop()
 
-#fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-#streamlit.write('Thanks for adding ', fruit_choice)
-#my_cur.execute("INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME ) VALUES ( '" + fruit_choice + "')")
+#
    
-
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("INSERT INTO FRUIT_LOAD_LIST (FRUIT_NAME ) VALUES ( '" + fruit_choice + "')")
+      return "Thanks for adding" + new_fruit
+   
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+if streamlit.button('Add a fruit to the List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   my_cnx.close()
+   streamlit.write('Thanks for adding ', back_from_function)
