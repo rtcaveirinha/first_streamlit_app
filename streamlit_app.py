f49eccca5d180a679d34ebcae432b712d8fdab89
rtcaveirinha <113922856+rtcaveirinha@users.noreply.github.com>
2022-09-22 10:15:41 +0100

    Update streamlit_app.py
    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Normalize json version of response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
