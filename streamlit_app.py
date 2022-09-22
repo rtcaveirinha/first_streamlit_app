baafcc3169fd7b5518b39f492a50131f01f47ff2
rtcaveirinha <113922856+rtcaveirinha@users.noreply.github.com>
2022-09-22 11:58:03 +0100

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,9 +62,7 @@ if streamlit.button('Get Fruit Load List'):
    my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
 
-#
    
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
